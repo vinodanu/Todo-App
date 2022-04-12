1d164aee687d3f8d00094e69f31e72a243da28d2
Vinod Anantha <Vinod.Anantha@BRILLIO.COM>
2022-04-12 13:41:42 +0530

    new commit

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 require("./db");
 const TodoRouter = require("./routes/todo.routes");
 
-app.use("/",(req, resp)  => {
-    resp.sendFile("./public/index.html")
-})
 
 const app = express();
-
 app.use(express.json());
+
+app.get("/",(req, resp)  => {
+    resp.sendFile(__dirname+"./public/index.html")
+})
+
 
 app.use("/api/todos", TodoRouter);
 
