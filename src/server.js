35b9b173957f14ba9bd9e11fd607e82dd0b27017
Vinod Anantha <Vinod.Anantha@BRILLIO.COM>
2022-04-25 15:18:36 +0530

    installed cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("./db");
 const TodoRouter = require("./routes/todo.routes");
+const cors = require('cors')
 
 
 const app = express();
+
+app.use(cors())
+
 app.use(express.json());
 
 app.get("/",(req, resp)  => {
